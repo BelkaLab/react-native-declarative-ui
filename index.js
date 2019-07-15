1b5549650771206371e1d78491e9e32d673eb1f4
frizzonelli <fabrizio.rizzonelli@gmail.com>
2019-07-15 18:32:18 +0200

    fixed index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { ComposableForm } from './src/ComposableForm';
+import { KeyboardAvoidingScrollView } from './src/KeyboardAvoidingScrollView';
 import SharedOptions from './src/options/SharedOptions';
 
-export { ComposableForm, SharedOptions };
+export { ComposableForm, KeyboardAvoidingScrollView, SharedOptions };
 
